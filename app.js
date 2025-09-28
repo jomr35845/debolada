cc2d9918bbeb54a494af3bdcb8a8a4229471c77e
jomr35845 <jomr35845@gmail.com>
2025-09-28 14:16:22 -0600

    Actualizar app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // Seleccionamos el botón
-    const boton = document.getElementById("irPagina");
+    const boton = document.getElementById("b-king");
 
     // Agregamos el event listener
     boton.addEventListener("click", () => {
       // Redirigir a otra página
-      window.location.href = "https://www.google.com";
+      window.location.href = "https://jomr35845.github.io/BurgerKing/";
     });
