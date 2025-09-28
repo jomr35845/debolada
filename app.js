f8f148f324e0b38e18a674fed804845b30f3868f
jomr35845 <jomr35845@gmail.com>
2025-09-28 14:14:35 -0600

    Actualizar app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-let a = 21;
+// Seleccionamos el botón
+    const boton = document.getElementById("irPagina");
+
+    // Agregamos el event listener
+    boton.addEventListener("click", () => {
+      // Redirigir a otra página
+      window.location.href = "https://www.google.com";
+    });
